b44457820ce754084c398b1be5f1937f483cda90
Ted Heikkila <ted.heikkila@gmail.com>
2021-08-15 14:01:53 -0500

    downgraded apollo server; testing queries/muts in graphql

--- server/server.js
+++ server/server.js
@@ -25,11 +25,9 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.use(routes);
-
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
+    console.log(`API server running on port ${PORT}`);
     // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
